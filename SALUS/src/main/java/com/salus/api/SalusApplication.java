ee07ab1824f2200bc17fbcc4bbf0e6822678416b
unknown <paschoaloliveira@hotmail.com>
2021-01-04 17:10:40 -0300

    Commit inicial

---
+++ SALUS/src/main/java/com/salus/api/SalusApplication.java
@@ -0,0 +1,13 @@
+package com.salus.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalusApplication.class, args);
+	}
+
+}
