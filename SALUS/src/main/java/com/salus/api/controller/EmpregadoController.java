1c82e3532a611c4009096d53bdadaa74ed8a4f2c
Paschoal <paschoaloliveira@hotmail.com>
2021-01-06 01:46:36 -0300

    Inserção de Empregados

---
+++ SALUS/src/main/java/com/salus/api/controller/EmpregadoController.java
@@ -0,0 +1,53 @@
+package com.salus.api.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.salus.api.model.Empregado;
+
+@RestController
+@RequestMapping(value = "/empregados")
+public class EmpregadoController {
+
+	
+	@GetMapping
+	public List<Empregado> get() {
+		
+		List<Empregado> empregados = new ArrayList<Empregado>();
+		
+		empregados.add(new Empregado("Rafael",10000.00));
+		empregados.add(new Empregado("Juliano",12000.00));
+		empregados.add(new Empregado("Gumersindo",14000.00));
+		empregados.add(new Empregado("Amaral",8000.00));
+		empregados.add(new Empregado("Guilherme",9000.00));
+		
+		return empregados;
+	}
+	
+	@RequestMapping("getStream")
+	public List<Empregado> getStream() {
+		
+		List<Empregado> empregados = new ArrayList<Empregado>();
+		
+		empregados.add(new Empregado("Rafael",10000.00));
+		empregados.add(new Empregado("Juliano",12000.00));
+		empregados.add(new Empregado("Raimundo",12000.00));
+		empregados.add(new Empregado("Gumersindo",14000.00));
+		empregados.add(new Empregado("Amaral",8000.00));
+		empregados.add(new Empregado("Guilherme",9000.00));
+		empregados.add(new Empregado("Rafael",12000.00));
+		
+		empregados = empregados.stream().filter(empregado -> empregado.getNome().equals("Rafael"))
+				.collect(Collectors.toList());
+		
+		Optional<Empregado> empregado = empregados.stream().findFirst();
+				
+		return empregados;
+	}
+}
---
+++ SALUS/src/main/java/com/salus/api/model/Empregado.java
@@ -0,0 +1,28 @@
+package com.salus.api.model;
+
+public class Empregado {
+
+	String nome;
+	
+	Double salario;
+
+	public Empregado(String nome, Double salario) {
+		this.nome = nome;
+		this.salario = salario;
+	}
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+}
