e23afc36e57b2da30fab14f0ac5636fe90af6340
Emanuel Cerqueira <emanuel.cerqueira@solutis.com.br>
2021-01-07 08:43:51 -0300

    Exercício Streams

--- SALUS/src/main/java/com/salus/api/controller/EmpregadoController.java
+++ SALUS/src/main/java/com/salus/api/controller/EmpregadoController.java
@@ -1,6 +1,7 @@
 package com.salus.api.controller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -43,11 +44,37 @@ public class EmpregadoController {
 		empregados.add(new Empregado("Guilherme",9000.00));
 		empregados.add(new Empregado("Rafael",12000.00));
 		
-		empregados = empregados.stream().filter(empregado -> empregado.getNome().equals("Rafael"))
+		List<Empregado> empregadosComSalario12000 = empregados
+				.stream()
+				.filter(empregado -> empregado.getSalario() == 12000.00)
 				.collect(Collectors.toList());
 		
-		Optional<Empregado> empregado = empregados.stream().findFirst();
-				
+		System.out.println("Empregados com salário igual a 12000:");
+		empregadosComSalario12000.stream().forEach(empregado -> System.out.println(empregado.getNome()));
+		
+		List<Empregado> empregadosComSalario12000ENomeComecandoComR = empregados
+				.stream()
+				.filter(empregado -> empregado.getSalario() == 12000.00)
+				.filter(empregado -> empregado.getNome().startsWith("R"))
+				.collect(Collectors.toList());
+		
+		System.out.println("\nEmpregados com salário igual a 12000 e comecem com a letra R");
+		empregadosComSalario12000ENomeComecandoComR.stream().forEach(empregado -> System.out.println(empregado.getNome()));
+		
+		Optional<Empregado> empregadoComMaiorSalario = empregados.stream().max(Comparator.comparingDouble(Empregado::getSalario));
+		
+		if (empregadoComMaiorSalario.isPresent()) {
+			System.out.println("\nEmpregado com maior salário:");
+			System.out.println(empregadoComMaiorSalario.get().getNome());
+		}
+		
+		empregados = empregados
+						.stream()
+						.map(empregado -> {
+							empregado.setSalario(8000.00);
+							return empregado;
+						}).collect(Collectors.toList());
+		
 		return empregados;
 	}
 }
